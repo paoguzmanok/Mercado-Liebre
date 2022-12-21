801355d410eb0ee845d182e9c9ce283662ab0879
paoguzmanok <paoguzmanok@gmail.com>
2022-12-21 17:50:29 -0300

    Agrega rutas

--- app.js
+++ app.js
@@ -8,5 +8,11 @@ app.listen(3000, () => {
 const publicpath = path.join(__dirname, "./public");
 app.use(express.static(publicpath));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/home.html"));
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
 });
